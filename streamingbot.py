20f83559a006b52d1334293b16460b2b42c8a429
yahiamr <146677961+yahiamr@users.noreply.github.com>
2024-05-09 23:34:08 +0300

    Update streamingbot.py

--- streamingbot.py
+++ streamingbot.py
@@ -14,7 +14,7 @@ for message in st.session_state.messages:
         st.markdown(message["content"])
 
 # Accept user input
-if prompt := st.chat_input("What is up?"):
+if prompt := st.chat_input("fuck u"):
     # Display user message in chat message container
     with st.chat_message("user"):
         st.markdown(prompt)
